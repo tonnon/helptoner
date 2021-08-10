a95f4ad2a356a6a26769ce641ffae1d2ceca81fd
tonnon <lucastonnon@gmail.com>
2021-08-10 15:06:17 -0300

    Updates

---
+++ precache-manifest.cdb42f63ba81ccbec5799644b516b075.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2ad5e6f6484b53efeda6795af96f968",
+    "url": "/helptoner/index.html"
+  },
+  {
+    "revision": "74fa0707c4bbd595616b",
+    "url": "/helptoner/static/css/main.7a7ddcf2.chunk.css"
+  },
+  {
+    "revision": "6e3010e6cca4bd44ca47",
+    "url": "/helptoner/static/js/2.c5326c21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/helptoner/static/js/2.c5326c21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74fa0707c4bbd595616b",
+    "url": "/helptoner/static/js/main.19936094.chunk.js"
+  },
+  {
+    "revision": "482b62d0ec8a4f62cd3d",
+    "url": "/helptoner/static/js/runtime-main.5c6f9a15.js"
+  }
+]);
